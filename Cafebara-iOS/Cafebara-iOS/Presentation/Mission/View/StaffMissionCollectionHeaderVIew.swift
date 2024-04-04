4406915490cd76edc0d29ebb6e77143d0816a386
Minsoo Kang <alstn38@naver.com>
2024-04-04 19:48:26 +0900

    [Feat] #22 - 미션 뷰 CollectionHeaderView UI 구현

--- Cafebara-iOS/Cafebara-iOS/Presentation/Mission/View/StaffMissionCollectionHeaderVIew.swift
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Mission/View/StaffMissionCollectionHeaderVIew.swift
@@ -1,8 +1,73 @@
 //
-//  StaffMissionCollectionHeaderVIew.swift
+//  StaffMissionCollectionHeaderView.swift
 //  Cafebara-iOS
 //
-//  Created by 강민수 on 4/3/24.
+//  Created by 강민수 on 4/2/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class StaffMissionCollectionHeaderView: UICollectionReusableView, UICollectionHeaderViewRegisterable {
+    
+    // MARK: - Properties
+    
+    static let isFromNib: Bool = false
+    
+    // MARK: - UI Components
+    
+    private let dateLabel = UILabel()
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setStyle()
+        setHierarchy()
+        setLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Extensions
+
+private extension StaffMissionCollectionHeaderView {
+    
+    func setUI() {
+        backgroundColor = .clear
+    }
+    
+    func setStyle() {
+        dateLabel.do {
+            $0.font = .fontBara(.title3)
+            $0.asLineHeight(.title3)
+            $0.textColor = .gray2
+        }
+    }
+    
+    func setHierarchy() {
+        addSubview(dateLabel)
+    }
+    
+    func setLayout() {
+        dateLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().inset(20)
+        }
+    }
+}
+
+extension StaffMissionCollectionHeaderView {
+    
+    func configureView(data: String) {
+        dateLabel.text = data
+    }
+}
