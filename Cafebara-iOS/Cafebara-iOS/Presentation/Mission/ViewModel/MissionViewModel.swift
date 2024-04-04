c85eda0f6b23ee631f91de163c416593bcf5fa42
Minsoo Kang <alstn38@naver.com>
2024-04-04 19:51:53 +0900

    [Feat] #22 - MissionViewModel 구현

--- Cafebara-iOS/Cafebara-iOS/Presentation/Mission/ViewModel/MissionViewModel.swift
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Mission/ViewModel/MissionViewModel.swift
@@ -5,4 +5,64 @@
 //  Created by 강민수 on 4/1/24.
 //
 
-import Foundation
+import UIKit
+
+import Moya
+import RxSwift
+import RxRelay
+
+protocol MissionViewModelInputs {
+}
+
+protocol MissionViewModelOutputs {
+    var todayDate: BehaviorSubject<String> { get }
+    var staffMissionInfo: BehaviorSubject<[SectionOfStaffMission]> { get }
+}
+
+protocol MissionViewModelType {
+    var inputs: MissionViewModelInputs { get }
+    var outputs: MissionViewModelOutputs { get }
+}
+
+final class MissionViewModel: MissionViewModelInputs, MissionViewModelOutputs, MissionViewModelType {
+    
+    var inputs: MissionViewModelInputs { return self }
+    var outputs: MissionViewModelOutputs { return self }
+    
+    private let disposeBag = DisposeBag()
+    
+    // input
+    
+    // output
+    // TODO: 서버 통신시 PublishSubject로 변경하기
+    var todayDate = BehaviorSubject<String>(value: "")
+    var staffMissionInfo = BehaviorSubject<[SectionOfStaffMission]>(value: [])
+    
+    init() {
+        var staffMissionDTO: [StaffMissionInfo] = [
+            StaffMissionInfo(name: "강민수", workKeyword: "오픈", workKeywordTextColor: "#FF4F4F", workKeywordBackColor: "#FFF3F3", startTime: "9:00", endTiem: "13:00"),
+            StaffMissionInfo(name: "고아라", workKeyword: "오픈", workKeywordTextColor: "#FF4F4F", workKeywordBackColor: "#FFF3F3", startTime: "9:00", endTiem: "13:00"),
+            StaffMissionInfo(name: "방민지", workKeyword: "미들", workKeywordTextColor: "#7827C9", workKeywordBackColor: "#F6F1FD", startTime: "12:00", endTiem: "16:00"),
+            StaffMissionInfo(name: "김가현", workKeyword: "미들2", workKeywordTextColor: "#1F9BB6", workKeywordBackColor: "#EAFBFA", startTime: "14:00", endTiem: "18:00"),
+            StaffMissionInfo(name: "김보연", workKeyword: "마감", workKeywordTextColor: "#6CB731", workKeywordBackColor: "#F6FEF3", startTime: "18:00", endTiem: "24:00"),
+            StaffMissionInfo(name: "강민수", workKeyword: "오픈", workKeywordTextColor: "#FF4F4F", workKeywordBackColor: "#FFF3F3", startTime: "9:00", endTiem: "13:00"),
+            StaffMissionInfo(name: "고아라", workKeyword: "오픈", workKeywordTextColor: "#FF4F4F", workKeywordBackColor: "#FFF3F3", startTime: "9:00", endTiem: "13:00"),
+            StaffMissionInfo(name: "방민지", workKeyword: "미들", workKeywordTextColor: "#7827C9", workKeywordBackColor: "#F6F1FD", startTime: "12:00", endTiem: "16:00"),
+            StaffMissionInfo(name: "김가현", workKeyword: "미들2", workKeywordTextColor: "#1F9BB6", workKeywordBackColor: "#EAFBFA", startTime: "14:00", endTiem: "18:00"),
+            StaffMissionInfo(name: "김보연", workKeyword: "마감", workKeywordTextColor: "#6CB731", workKeywordBackColor: "#F6FEF3", startTime: "18:00", endTiem: "24:00")
+        ]
+        staffMissionInfo.onNext([SectionOfStaffMission(header: getCurrentDate(), items: staffMissionDTO)])
+    }
+}
+
+extension MissionViewModel {
+    
+    func getCurrentDate() -> String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "Ko")
+        formatter.dateFormat = "MM.dd E요일"
+        let currentDate = formatter.string(from: Date())
+        
+        return currentDate
+    }
+}
