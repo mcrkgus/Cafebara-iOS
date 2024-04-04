6e34346166d45142981894823ab13eac7092f4d2
Minsoo Kang <alstn38@naver.com>
2024-04-04 19:52:13 +0900

    [Feat] #22 - MissionViewController 구현

--- Cafebara-iOS/Cafebara-iOS/Presentation/Mission/ViewController/MissionViewController.swift
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Mission/ViewController/MissionViewController.swift
@@ -7,18 +7,25 @@
 
 import UIKit
 
+import RxCocoa
+import RxDataSources
+import RxSwift
+
 final class MissionViewController: UIViewController {
     
     // MARK: - Properties
     
+    private var viewModel = MissionViewModel()
+    private let disposeBag = DisposeBag()
     
     // MARK: - UI Components
     
+    private let missionView = MissionView()
     
     // MARK: - Life Cycles
     
     override func loadView() {
-        
+        view = missionView
     }
     
     override func viewDidLoad() {
@@ -26,23 +33,35 @@ final class MissionViewController: UIViewController {
         
         setUI()
         bindViewModel()
-        setDelegate()
     }
 }
 
 // MARK: - Extensions
 
 extension MissionViewController {
-
+    
     func setUI() {
-        
-    }
-
-    func bindViewModel() {
-	
+        self.navigationController?.navigationBar.isHidden = true
     }
     
-    func setDelegate() {
+    func bindViewModel() {
+        let dataSource = RxCollectionViewSectionedReloadDataSource<SectionOfStaffMission>(
+            configureCell: { (_, collectionView, indexPath, item) in
+                let cell = StaffMissionCollectionViewCell.dequeueReusableCell(collectionView: collectionView, indexPath: indexPath)
+                cell.configureCell(data: item)
+                
+                return cell
+            },
+            configureSupplementaryView: { (dataSource, collectionView, _, indexPath) in
+                let headerView = StaffMissionCollectionHeaderView.dequeueReusableHeaderView(collectionView: collectionView, indexPath: indexPath)
+                headerView.configureView(data: dataSource[indexPath.section].header)
+                
+                return headerView
+            }
+        )
         
+        viewModel.outputs.staffMissionInfo
+            .bind(to: missionView.staffMissionCollectionView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
     }
 }
