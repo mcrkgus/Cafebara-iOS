3fa6311595ccd67a3bea1a19e24e942285a5b702
Minsoo Kang <alstn38@naver.com>
2024-04-04 19:47:53 +0900

    [Feat] #22 - 미션 뷰 UI 구현

--- Cafebara-iOS/Cafebara-iOS/Presentation/Mission/View/MissionView.swift
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Mission/View/MissionView.swift
@@ -8,15 +8,19 @@
 import UIKit
 
 import SnapKit
-Import Then
+import Then
 
 final class MissionView: UIView {
-
-    // MARK: - Properties
     
+    // MARK: - Properties
     
     // MARK: - UI Components
     
+    let navigationBar = CustomNavigationView()
+    private let routineImageView = UIImageView()
+    private let routineLabel = UILabel()
+    let routineButton = UIButton()
+    let staffMissionCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
     
     // MARK: - Life Cycles
     
@@ -27,7 +31,7 @@ final class MissionView: UIView {
         setStyle()
         setHierarchy()
         setLayout()
-        setRegisterCell()
+        setRegister()
     }
     
     @available(*, unavailable)
@@ -39,31 +43,88 @@ final class MissionView: UIView {
 // MARK: - Extensions
 
 private extension MissionView {
-
+    
     func setUI() {
-        
+        backgroundColor = .backgroundBara
     }
-
+    
     func setStyle() {
+        navigationBar.do {
+            $0.isTitleLabelIncluded = true
+            $0.isModifyButtonIncluded = true
+            $0.titleLabelText = I18N.Mission.missionNavigationTitle
+        }
         
+        routineImageView.do {
+            $0.image = UIImage(resource: .graphicSpeechBubble)
+        }
+        
+        routineLabel.do {
+            $0.text = I18N.Mission.routineTitle
+            $0.font = .fontBara(.caption2)
+            $0.asLineHeight(.caption2)
+            $0.textColor = .gray3
+        }
+        
+        routineButton.do {
+            $0.setImage(.icSpeechBubbleDelete16Gray3, for: .normal)
+        }
+        
+        staffMissionCollectionView.do {
+            let layout = UICollectionViewFlowLayout()
+            layout.scrollDirection = .vertical
+            layout.minimumLineSpacing = 11.0
+            layout.itemSize = CGSize(width: SizeLiterals.Screen.screenWidth - 40, height: 88)
+            layout.headerReferenceSize = CGSize(width: SizeLiterals.Screen.screenWidth, height: 34)
+            layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 34, right: 0)
+            
+            $0.collectionViewLayout = layout
+            $0.backgroundColor = .clear
+            $0.showsVerticalScrollIndicator = false
+        }
     }
     
     func setHierarchy() {
-
+        addSubviews(navigationBar,
+                    staffMissionCollectionView,
+                    routineImageView,
+                    routineLabel,
+                    routineButton)
     }
     
     func setLayout() {
-
+        navigationBar.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+        }
+        
+        routineImageView.snp.makeConstraints {
+            $0.top.equalTo(navigationBar).offset(49)
+            $0.trailing.equalToSuperview().inset(10)
+            $0.width.equalTo(169)
+            $0.height.equalTo(34)
+        }
+        
+        routineLabel.snp.makeConstraints {
+            $0.bottom.equalTo(routineImageView).inset(7)
+            $0.leading.equalTo(routineImageView).offset(8)
+        }
+        
+        routineButton.snp.makeConstraints {
+            $0.centerY.equalTo(routineLabel)
+            $0.trailing.equalTo(routineImageView).inset(6)
+            $0.size.equalTo(16)
+        }
+        
+        staffMissionCollectionView.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(safeAreaLayoutGuide)
+        }
     }
     
-    func setRegisterCell() {
-        
-    }
-}
-
-extension MissionView {
-
-    func configureView() {
-        
+    func setRegister() {
+        StaffMissionCollectionHeaderView.register(target: staffMissionCollectionView)
+        StaffMissionCollectionViewCell.register(target: staffMissionCollectionView)
     }
 }
