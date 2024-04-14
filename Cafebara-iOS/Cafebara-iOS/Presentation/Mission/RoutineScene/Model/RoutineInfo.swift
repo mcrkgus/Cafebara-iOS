b6fc9bb61d9eeba0df88d007967aad425d80b7d2
Minsoo Kang <alstn38@naver.com>
2024-04-14 23:34:22 +0900

    [Feat] #32 - Routine Model 구현

--- Cafebara-iOS/Cafebara-iOS/Presentation/Mission/RoutineScene/Model/RoutineInfo.swift
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Mission/RoutineScene/Model/RoutineInfo.swift
@@ -2,7 +2,30 @@
 //  RoutineInfo.swift
 //  Cafebara-iOS
 //
-//  Created by 강민수 on 4/14/24.
+//  Created by 강민수 on 4/8/24.
 //
 
 import Foundation
+
+import RxDataSources
+
+struct RoutineInfo: Codable {
+    let routineKeyword: String
+    let routineKeywordTextColor: String
+    let routineKeywordBackColor: String
+    let routineExplain: String
+}
+
+struct SectionOfRoutine {
+    var items: [Item]
+}
+
+extension SectionOfRoutine: SectionModelType {
+    
+    typealias Item = RoutineInfo
+    
+    init(original: SectionOfRoutine, items: [RoutineInfo]) {
+        self = original
+        self.items = items
+    }
+}
