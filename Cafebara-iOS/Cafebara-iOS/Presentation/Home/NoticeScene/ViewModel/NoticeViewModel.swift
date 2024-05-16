afe7505e866beb64627befdc80d2c969e38ce435
bangMinji98 <whoru9806@gmail.com>
2024-05-17 00:24:23 +0900

    [Feat] #24 NoticeViewModel 구현
    추후 수정 예정

--- Cafebara-iOS/Cafebara-iOS/Presentation/Home/NoticeScene/ViewModel/NoticeViewModel.swift
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Home/NoticeScene/ViewModel/NoticeViewModel.swift
@@ -5,4 +5,38 @@
 //  Created by 방민지 on 5/8/24.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxRelay
+
+protocol NoticeViewModelInputs {
+    
+    func configureData()
+}
+
+protocol NoticeViewModelOutputs {
+    
+    var noticeInfoData: BehaviorRelay<[NoticeInfo]> { get }
+}
+
+protocol NoticeViewModelType {
+    
+    var inputs: NoticeViewModelInputs { get }
+    var outputs: NoticeViewModelOutputs { get }
+}
+
+final class NoticeViewModel: NoticeViewModelInputs, NoticeViewModelOutputs, NoticeViewModelType {
+    
+    var inputs: NoticeViewModelInputs { return self }
+    var outputs: NoticeViewModelOutputs { return self }
+    
+    private let disposeBag = DisposeBag()
+    
+    // Inputs
+    func configureData() {
+        // TODO: 서버 통신 데이터를 가져오는 로직
+    }
+    
+    // Outputs
+    var noticeInfoData: BehaviorRelay<[NoticeInfo]> = BehaviorRelay<[NoticeInfo]>(value: NoticeInfo.noticeDummy())
+}
