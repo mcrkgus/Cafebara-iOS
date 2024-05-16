f6a21258f7ebb783928d88ff0fc2d032e9f36939
bangMinji98 <whoru9806@gmail.com>
2024-05-17 00:24:01 +0900

    Feat [#24] NoticeViewController 구현

--- Cafebara-iOS/Cafebara-iOS/Presentation/Home/NoticeScene/ViewController/NoticeViewController.swift
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Home/NoticeScene/ViewController/NoticeViewController.swift
@@ -7,10 +7,16 @@
 
 import UIKit
 
+import RxCocoa
+import RxDataSources
+import RxSwift
+
 final class NoticeViewController: UIViewController {
     
     // MARK: - Properties
     
+    private let viewModel = NoticeViewModel()
+    private let disposeBag = DisposeBag()
     
     // MARK: - UI Components
     
@@ -23,6 +29,7 @@ final class NoticeViewController: UIViewController {
     }
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         setUI()
@@ -36,14 +43,21 @@ final class NoticeViewController: UIViewController {
 extension NoticeViewController {
 
     func setUI() {
+        
         self.navigationController?.navigationBar.isHidden = true
+        noticeView.navigationBar.backButtonAction = {
+            self.navigationController?.popViewController(animated: true)
+        }
     }
 
     func bindViewModel() {
-	
-    }
-    
-    func setDelegate() {
         
+        viewModel.outputs.noticeInfoData
+            .bind(to: noticeView.noticeCollectionView.rx
+                .items(cellIdentifier: NoticeCollectionViewCell.className,
+                       cellType: NoticeCollectionViewCell.self)) { (_, model, cell) in
+                cell.configureCell(data: model)
+            }
+            .disposed(by: disposeBag)
     }
 }
