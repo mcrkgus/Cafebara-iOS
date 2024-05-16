d8b4f35ae6eed418c11c358a47484bd938a3b195
bangMinji98 <whoru9806@gmail.com>
2024-05-17 00:22:31 +0900

    [Feat] #24 임시데이터 더미

--- Cafebara-iOS/Cafebara-iOS/Presentation/Home/NoticeScene/Model/NoticeInfo.swift
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Home/NoticeScene/Model/NoticeInfo.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import Differentiator
+
+struct NoticeInfo: Codable {
+    let staffName: String
+    let date: [Int]
+    let noticeType: String
+}
+
+extension NoticeInfo {
+    static func noticeDummy() -> [NoticeInfo] {
+        return [
+            NoticeInfo(staffName: "", date: [1998, 06, 22], noticeType: "a"),
+            NoticeInfo(staffName: "", date: [1998, 06, 22], noticeType: "a"),
+            NoticeInfo(staffName: "", date: [2024, 06, 22], noticeType: "a"),
+            NoticeInfo(staffName: "김가현", date: [2024, 06, 22], noticeType: "b"),
+            NoticeInfo(staffName: "고아라", date: [1998, 06, 22], noticeType: "b"),
+            NoticeInfo(staffName: "", date: [1998, 06, 22], noticeType: "a"),
+            NoticeInfo(staffName: "강민수", date: [1998, 06, 22], noticeType: "b"),
+            NoticeInfo(staffName: "", date: [2026, 06, 22], noticeType: "a"),
+            NoticeInfo(staffName: "김보연", date: [1998, 06, 22], noticeType: "b")
+        ]
+    }
+}
