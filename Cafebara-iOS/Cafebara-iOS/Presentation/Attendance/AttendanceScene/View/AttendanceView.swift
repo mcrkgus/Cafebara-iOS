4553e88a64a525da4896dc89a501ba47b3890b5f
bangMinji98 <whoru9806@gmail.com>
2024-05-21 19:56:56 +0900

    [Add] #40 출퇴근뷰 Ui 관련 파일 추가

---
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Attendance/AttendanceScene/View/AttendanceView.swift
@@ -0,0 +1,69 @@
+//
+//  AttendanceView.swift
+//  Cafebara-iOS
+//
+//  Created by 방민지 on 5/20/24.
+//
+
+import UIKit
+
+import SnapKit
+Import Then
+
+final class AttendanceView: UIView {
+
+    // MARK: - Properties
+    
+    
+    // MARK: - UI Components
+    
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setStyle()
+        setHierarchy()
+        setLayout()
+        setRegisterCell()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Extensions
+
+private extension AttendanceView {
+
+    func setUI() {
+        
+    }
+
+    func setStyle() {
+        
+    }
+    
+    func setHierarchy() {
+
+    }
+    
+    func setLayout() {
+
+    }
+    
+    func setRegisterCell() {
+        
+    }
+}
+
+extension AttendanceView {
+
+    func configureView() {
+        
+    }
+}
---
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Attendance/AttendanceScene/ViewController/AttendanceViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AttendanceViewController.swift
+//  Cafebara-iOS
+//
+//  Created by 방민지 on 5/20/24.
+//
+
+import UIKit
+
+final class AttendanceViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - UI Components
+    
+    
+    // MARK: - Life Cycles
+    
+    override func loadView() {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        bindViewModel()
+        setDelegate()
+    }
+}
+
+// MARK: - Extensions
+
+extension AttendanceViewController {
+
+    func setUI() {
+        
+    }
+
+    func bindViewModel() {
+	
+    }
+    
+    func setDelegate() {
+        
+    }
+}
