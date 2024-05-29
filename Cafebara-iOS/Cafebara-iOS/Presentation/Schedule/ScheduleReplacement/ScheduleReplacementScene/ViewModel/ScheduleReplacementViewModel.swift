75de636bec15338eeed51f2db0089b85460b8c02
bangMinji98 <whoru9806@gmail.com>
2024-05-29 11:56:28 +0900

    [Feat] #42 - 대타수락뷰모델 구현

--- Cafebara-iOS/Cafebara-iOS/Presentation/Schedule/ScheduleReplacement/ScheduleReplacementScene/ViewModel/ScheduleReplacementViewModel.swift
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Schedule/ScheduleReplacement/ScheduleReplacementScene/ViewModel/ScheduleReplacementViewModel.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+import Moya
+import RxSwift
+import RxCocoa
+
+protocol ScheduleReplacementViewModelInputs {
+    func configureData()
+    func didTapRejectButton()
+    func didTapAcceptButton()
+}
+
+protocol ScheduleReplacementViewModelOutputs {
+    var scheduleReplacementInfoData: BehaviorRelay<[ScheduleReplacementInfo]> { get }
+    var rejectButtonTapResult: PublishRelay<String> { get }
+    var acceptButtonTapResult: PublishRelay<String> { get }
+}
+
+protocol ScheduleReplacementViewModelType {
+    var inputs: ScheduleReplacementViewModelInputs { get }
+    var outputs: ScheduleReplacementViewModelOutputs { get }
+}
+
+final class ScheduleReplacementViewModel: ScheduleReplacementViewModelInputs, ScheduleReplacementViewModelOutputs, ScheduleReplacementViewModelType {
+    var inputs: ScheduleReplacementViewModelInputs { return self }
+    var outputs: ScheduleReplacementViewModelOutputs {
+        return self
+    }
+    
+    private let disposeBag = DisposeBag()
+    
+    func configureData() {
+        // TODO: 서버 통신 데이터를 가져오는 로직
+    }
+    
+    func didTapRejectButton() {
+        rejectButtonTapResult.accept("거절버튼 눌림")
+    }
+    
+    func didTapAcceptButton() {
+        acceptButtonTapResult.accept("수락버튼 눌림")
+    }
+
+    var scheduleReplacementInfoData: BehaviorRelay<[ScheduleReplacementInfo]> =
+    BehaviorRelay<[ScheduleReplacementInfo]> (value:
+                                                ScheduleReplacementInfo.scheduleReplacementDummy())
+    
+    var rejectButtonTapResult: PublishRelay<String> = PublishRelay<String>()
+    var acceptButtonTapResult: PublishRelay<String> = PublishRelay<String>()
+}
