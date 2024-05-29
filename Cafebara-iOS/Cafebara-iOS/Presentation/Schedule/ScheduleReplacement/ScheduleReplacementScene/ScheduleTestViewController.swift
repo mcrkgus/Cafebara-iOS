6eede04df7badfb43176943360ca697dd1b3c1b1
bangMinji98 <whoru9806@gmail.com>
2024-05-29 11:53:39 +0900

    [Add] #42 - 대타수락뷰 관련 파일 추가

---
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Schedule/ScheduleReplacement/ScheduleReplacementScene/ScheduleTestViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ScheduleTestViewController.swift
+//  Cafebara-iOS
+//
+//  Created by 방민지 on 5/25/24.
+//
+
+import UIKit
+
+final class ScheduleTestViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - UI Components
+    
+    
+    // MARK: - Life Cycles
+    
+    override func loadView() {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        bindViewModel()
+        setDelegate()
+    }
+}
+
+// MARK: - Extensions
+
+extension ScheduleTestViewController {
+
+    func setUI() {
+        
+    }
+
+    func bindViewModel() {
+	
+    }
+    
+    func setDelegate() {
+        
+    }
+}
