aa9eb8ed10cada261ff1f4639f0baf48659a058a
bangMinji98 <whoru9806@gmail.com>
2024-05-29 11:57:15 +0900

    [Feat] #42 - 뷰 구현 확인을 위한 임시 뷰컨 생성

--- Cafebara-iOS/Cafebara-iOS/Presentation/Schedule/ScheduleReplacement/ScheduleReplacementScene/ScheduleTestViewController.swift
+++ Cafebara-iOS/Cafebara-iOS/Presentation/Schedule/ScheduleReplacement/ScheduleReplacementScene/ScheduleTestViewController.swift
@@ -7,18 +7,24 @@
 
 import UIKit
 
+import RxSwift
+import RxCocoa
+
 final class ScheduleTestViewController: UIViewController {
     
     // MARK: - Properties
     
+    let viewModel: ScheduleReplacementViewModelType = ScheduleReplacementViewModel()
+    let disposeBag = DisposeBag()
     
     // MARK: - UI Components
     
+    private let scheduleReplacementView = ScheduleReplacementView()
     
     // MARK: - Life Cycles
     
     override func loadView() {
-        
+        view = scheduleReplacementView
     }
     
     override func viewDidLoad() {
@@ -33,13 +39,45 @@ final class ScheduleTestViewController: UIViewController {
 // MARK: - Extensions
 
 extension ScheduleTestViewController {
-
+    
     func setUI() {
-        
+        self.navigationController?.navigationBar.isHidden = true
     }
-
+    
     func bindViewModel() {
-	
+        viewModel.outputs.scheduleReplacementInfoData
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] data in
+                guard let self = self else { return }
+                if let firstData = data.first {
+                    self.scheduleReplacementView.configureView(data: firstData)
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        scheduleReplacementView.rejectButton.rx.tap
+            .bind { [weak self] in
+                self?.viewModel.inputs.didTapRejectButton()
+            }
+            .disposed(by: disposeBag)
+        
+        scheduleReplacementView.acceptButton.rx.tap
+            .bind { [weak self] in
+                self?.viewModel.inputs.didTapAcceptButton()
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.outputs.rejectButtonTapResult
+            .subscribe(onNext: { result in
+                print(result)
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.outputs.acceptButtonTapResult
+            .subscribe(onNext: { result in
+                print(result)
+            })
+            .disposed(by: disposeBag)
     }
     
     func setDelegate() {
