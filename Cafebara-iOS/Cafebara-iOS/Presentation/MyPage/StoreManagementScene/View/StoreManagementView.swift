11a1804194bbc992708514b54b3ee5ef16519adb
BangMinJi <whoru9806@gmail.com>
2024-07-04 04:01:11 +0900

    [Feat] #44 - 업장관리뷰 구현

--- Cafebara-iOS/Cafebara-iOS/Presentation/MyPage/StoreManagementScene/View/StoreManagementView.swift
+++ Cafebara-iOS/Cafebara-iOS/Presentation/MyPage/StoreManagementScene/View/StoreManagementView.swift
@@ -12,11 +12,10 @@ import Then
 
 final class StoreManagementView: UIView {
 
-    // MARK: - Properties
-    
-    
     // MARK: - UI Components
-    
+    let navigationBar = CustomNavigationView()
+    let storeManageCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    let exitButton = CustomButton(status: false, type: .noBorder, title: I18N.StoreManagement.exitStoreButtonTitle)
     
     // MARK: - Life Cycles
     
@@ -27,7 +26,7 @@ final class StoreManagementView: UIView {
         setStyle()
         setHierarchy()
         setLayout()
-        setRegisterCell()
+        setRegister()
     }
     
     @available(*, unavailable)
@@ -38,32 +37,59 @@ final class StoreManagementView: UIView {
 
 // MARK: - Extensions
 
-private extension StoreManagementView {
-
+extension StoreManagementView {
+    
     func setUI() {
-        
+        backgroundColor = .backgroundBara
     }
-
+    
     func setStyle() {
+        navigationBar.do {
+            $0.isTitleLabelIncluded = true
+            $0.isBackButtonIncluded = true
+            $0.titleLabelText = I18N.StoreManagement.exitStoreTitle
+        }
         
+        storeManageCollectionView.do {
+            let layout = UICollectionViewFlowLayout()
+            layout.minimumInteritemSpacing = 0
+            layout.minimumLineSpacing = 12
+            layout.scrollDirection = .vertical
+            layout.headerReferenceSize = CGSize(width: SizeLiterals.Screen.screenWidth, height: 64)
+            layout.itemSize = CGSize(width: SizeLiterals.Screen.screenWidth - 40, height: 82)
+            $0.collectionViewLayout = layout
+            $0.showsVerticalScrollIndicator = false
+            $0.backgroundColor = .clear
+        }
     }
     
     func setHierarchy() {
-
+        addSubviews(navigationBar,
+                    storeManageCollectionView,
+                    exitButton)
     }
     
     func setLayout() {
-
+        navigationBar.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(safeAreaLayoutGuide)
+        }
+        
+        storeManageCollectionView.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.bottom.equalTo(exitButton.snp.top).offset(-26)
+        }
+        
+        exitButton.snp.makeConstraints {
+            $0.top.equalTo(storeManageCollectionView.snp.bottom).offset(26)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.bottom.equalTo(safeAreaLayoutGuide).offset(-22)
+        }
     }
     
-    func setRegisterCell() {
-        
-    }
-}
-
-extension StoreManagementView {
-
-    func configureView() {
-        
+    func setRegister() {
+        StoreManagementCollectionHeaderView.register(target: storeManageCollectionView)
+        StoreManagementCollectionViewCell.register(target: storeManageCollectionView)
     }
 }
