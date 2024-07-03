8220386be93633b4b2b318542a16dd280aa4570c
BangMinJi <whoru9806@gmail.com>
2024-07-04 04:02:05 +0900

    [Feat] #44 - 업장관리뷰 임시 info , 뷰모델 구현
    추후 서버 연결 시 수정 예정

--- Cafebara-iOS/Cafebara-iOS/Presentation/MyPage/StoreManagementScene/Model/StoreManagementInfo.swift
+++ Cafebara-iOS/Cafebara-iOS/Presentation/MyPage/StoreManagementScene/Model/StoreManagementInfo.swift
@@ -5,4 +5,26 @@
 //  Created by 방민지 on 5/30/24.
 //
 
-import Foundation
+import UIKit
+
+import RxDataSources
+
+struct StoreManagementInfo: Codable {
+    let storeName: String
+    let date: [Int]
+    let staffNum: Int
+}
+
+struct StoreInfoSection {
+    var header: UIView
+    var items: [Item]
+}
+
+extension StoreInfoSection: SectionModelType {
+    typealias Item = StoreManagementInfo
+    
+    init(original: StoreInfoSection, items: [StoreManagementInfo]) {
+        self = original
+        self.items = items
+    }
+}
